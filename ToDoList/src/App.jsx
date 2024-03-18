7ebfa56a900aaeee0291dfbf0802dd762a19177b
Ajay Saini <work.ajaysaini@gmail.com>
2024-03-18 18:48:57 +0530

    UpadateContexts

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
+import { ToDoProvider } from "./contexts"
 
 
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todos) => {
+    
+  }
 
   return (
-    <>
-    <h1 className="p-4 m-2 bg-blue-300 text-3xl text-center">To Do App</h1>
-    </>
+    <ToDoProvider value ={{todos, addTodo, deleteToDo, updateToDo, toggleComplete}}>
+    <div className="bg-[#152d5a] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-lg bg-blue-700 rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-6xl font-bold text-center mb-8 mt-6">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </ToDoProvider>
   )
 }
 
--- ToDoList/src/contexts/ToDoContexts.js
+++ ToDoList/src/contexts/ToDoContexts.js
@@ -1,4 +1,4 @@
-import { createContext, createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const ToDoContext = createContext({
     todos: [
